b305dd2e8f0506ce92d4094c44acfc135b68946e
pravinrajpaul <pravinrajpaul@gmail.com>
2024-03-11 15:12:38 +0530

    IOS Native Mobile Suite

---
+++ Test Suites/Mobile/IOSNative - Health.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>IOSNative - Health</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4cd0f00c-0760-4475-a1ba-4c239aa2c625</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb79a6ac-4bad-4eea-a7af-5d82849c9c9b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/IOSNative - Health</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
