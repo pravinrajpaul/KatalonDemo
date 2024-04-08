fe68066cdc709bd859fc416cd90f6e9a0cb6bbb5
pravi <pravi@Pravins-Win>
2024-04-08 14:02:39 +0530

    Geolocation test updated

--- Test Suites/Mobile/AndroidNative - Geolocation.ts
+++ Test Suites/Mobile/AndroidNative - Geolocation.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>a546b778-1972-4a20-9adc-fa54f37fb4e8</testSuiteGuid>
+   <testCaseLink>
+      <guid>9938f8e9-d5eb-441b-94bd-0f7cafd80e72</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mobile/AndroidNiteive - Geolocation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
