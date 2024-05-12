552851d1ff8bd954cddae54d69620b7c3b15a726
Pravin Raj Paul <pravinrajpaul@gmail.com>
2024-05-12 19:50:43 +0530

    Test suite for Salesforce

---
+++ Test Suites/Web/DesktopBrowser - Salesforce.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DesktopBrowser - Salesforce</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0783ca24-3a8d-42c8-8824-2f08c29ff99d</testSuiteGuid>
+   <testCaseLink>
+      <guid>a4b6967e-71a1-4f0e-8668-933ae68a952b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web/SalesForce/MainTest/Account Management</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4dc80374-57c7-4178-a8bf-2ff57b107bb4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web/SalesForce/MainTest/AppLauncher</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
