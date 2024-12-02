699fe122d127168c0646b223dda8603ef1d129d9
Pravin Raj Paul <pravinrajpaul@gmail.com>
2024-12-02 19:34:24 +0530

    New Test Cloud Collection

---
+++ Test Suites/Mobile/AndroidNativeCollection - Telegram.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>AndroidNativeCollection - Telegram</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>PARALLEL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>testCloudMobileDevice</key>
+                  <value>{
+  &quot;id&quot;: &quot;samsung_galaxy_s24&quot;,
+  &quot;isTablet&quot;: false,
+  &quot;manufacturer&quot;: [
+    &quot;Samsung&quot;
+  ],
+  &quot;name&quot;: &quot;Galaxy S24&quot;,
+  &quot;osVersion&quot;: &quot;14&quot;,
+  &quot;mobileDeviceId&quot;: &quot;Galaxy_S24_android_14&quot;,
+  &quot;capability&quot;: {
+    &quot;availability&quot;: &quot;HIGH&quot;
+  },
+  &quot;os&quot;: &quot;Android&quot;,
+  &quot;platformName&quot;: &quot;ANDROID&quot;,
+  &quot;timeout&quot;: 15,
+  &quot;usingTunnel&quot;: false
+}</value>
+               </entry>
+               <entry>
+                  <key>testCloudSessionType</key>
+                  <value>MOBILE_NATIVE</value>
+               </entry>
+               <entry>
+                  <key>testCloudOverrideMobileApplication</key>
+                  <value>{&quot;id&quot;:&quot;5589c05c-db25-432f-8d3d-0ea060f3d491&quot;,&quot;organizationId&quot;:&quot;1157462&quot;,&quot;name&quot;:&quot;Telegram&quot;,&quot;platform&quot;:&quot;ANDROID&quot;,&quot;status&quot;:&quot;READY&quot;,&quot;size&quot;:74849307,&quot;storagePath&quot;:&quot;7ec22c20-8674-4bdb-9ef3-8db1134245ab&quot;,&quot;uploadedDate&quot;:&quot;2024-11-08T07:15:35.28657&quot;,&quot;createdDate&quot;:&quot;2024-11-08T07:15:35.28657&quot;,&quot;updatedDate&quot;:&quot;2024-11-08T07:16:06.731081&quot;,&quot;fileName&quot;:&quot;Telegram.apk&quot;,&quot;metadata&quot;:{&quot;identifier&quot;:&quot;org.telegram.messenger.web&quot;,&quot;name&quot;:&quot;Telegram&quot;,&quot;version&quot;:&quot;11.3.4&quot;}}</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>TestCloud</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Mobile/AndroidNative - Telegram</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>testCloudMobileDevice</key>
+                  <value>{
+  &quot;id&quot;: &quot;google_pixel_8&quot;,
+  &quot;isTablet&quot;: false,
+  &quot;manufacturer&quot;: [
+    &quot;Google&quot;
+  ],
+  &quot;name&quot;: &quot;Pixel 8&quot;,
+  &quot;osVersion&quot;: &quot;14&quot;,
+  &quot;mobileDeviceId&quot;: &quot;Pixel_8_android_14&quot;,
+  &quot;capability&quot;: {
+    &quot;availability&quot;: &quot;HIGH&quot;
+  },
+  &quot;os&quot;: &quot;Android&quot;,
+  &quot;platformName&quot;: &quot;ANDROID&quot;,
+  &quot;timeout&quot;: 15,
+  &quot;usingTunnel&quot;: false
+}</value>
+               </entry>
+               <entry>
+                  <key>testCloudSessionType</key>
+                  <value>MOBILE_NATIVE</value>
+               </entry>
+               <entry>
+                  <key>testCloudOverrideMobileApplication</key>
+                  <value>{&quot;id&quot;:&quot;5589c05c-db25-432f-8d3d-0ea060f3d491&quot;,&quot;organizationId&quot;:&quot;1157462&quot;,&quot;name&quot;:&quot;Telegram&quot;,&quot;platform&quot;:&quot;ANDROID&quot;,&quot;status&quot;:&quot;READY&quot;,&quot;size&quot;:74849307,&quot;storagePath&quot;:&quot;7ec22c20-8674-4bdb-9ef3-8db1134245ab&quot;,&quot;uploadedDate&quot;:&quot;2024-11-08T07:15:35.28657&quot;,&quot;createdDate&quot;:&quot;2024-11-08T07:15:35.28657&quot;,&quot;updatedDate&quot;:&quot;2024-11-08T07:16:06.731081&quot;,&quot;fileName&quot;:&quot;Telegram.apk&quot;,&quot;metadata&quot;:{&quot;identifier&quot;:&quot;org.telegram.messenger.web&quot;,&quot;name&quot;:&quot;Telegram&quot;,&quot;version&quot;:&quot;11.3.4&quot;}}</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>TestCloud</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Mobile/AndroidNative - Telegram</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>testCloudMobileDevice</key>
+                  <value>{
+  &quot;id&quot;: &quot;samsung_galaxy_s23_ultra&quot;,
+  &quot;isTablet&quot;: false,
+  &quot;manufacturer&quot;: [
+    &quot;Samsung&quot;
+  ],
+  &quot;name&quot;: &quot;Galaxy S23 Ultra&quot;,
+  &quot;osVersion&quot;: &quot;13&quot;,
+  &quot;mobileDeviceId&quot;: &quot;Galaxy_S23_Ultra_android_13&quot;,
+  &quot;capability&quot;: {
+    &quot;availability&quot;: &quot;HIGH&quot;
+  },
+  &quot;os&quot;: &quot;Android&quot;,
+  &quot;platformName&quot;: &quot;ANDROID&quot;,
+  &quot;timeout&quot;: 15,
+  &quot;usingTunnel&quot;: false
+}</value>
+               </entry>
+               <entry>
+                  <key>testCloudSessionType</key>
+                  <value>MOBILE_NATIVE</value>
+               </entry>
+               <entry>
+                  <key>testCloudOverrideMobileApplication</key>
+                  <value>{&quot;id&quot;:&quot;5589c05c-db25-432f-8d3d-0ea060f3d491&quot;,&quot;organizationId&quot;:&quot;1157462&quot;,&quot;name&quot;:&quot;Telegram&quot;,&quot;platform&quot;:&quot;ANDROID&quot;,&quot;status&quot;:&quot;READY&quot;,&quot;size&quot;:74849307,&quot;storagePath&quot;:&quot;7ec22c20-8674-4bdb-9ef3-8db1134245ab&quot;,&quot;uploadedDate&quot;:&quot;2024-11-08T07:15:35.28657&quot;,&quot;createdDate&quot;:&quot;2024-11-08T07:15:35.28657&quot;,&quot;updatedDate&quot;:&quot;2024-11-08T07:16:06.731081&quot;,&quot;fileName&quot;:&quot;Telegram.apk&quot;,&quot;metadata&quot;:{&quot;identifier&quot;:&quot;org.telegram.messenger.web&quot;,&quot;name&quot;:&quot;Telegram&quot;,&quot;version&quot;:&quot;11.3.4&quot;}}</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>TestCloud</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Mobile/AndroidNative - Telegram</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
